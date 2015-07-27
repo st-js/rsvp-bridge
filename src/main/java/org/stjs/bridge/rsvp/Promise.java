d7aa1541f2c8df2fb637a4e51ae0fc94ea0b1a08
Nicolas Piguet <nicolas.piguet@swissquote.ch>
2015-07-27 19:05:04 +0200

    Started the documentation of the Promise class

--- src/main/java/org/stjs/bridge/rsvp/Promise.java
+++ src/main/java/org/stjs/bridge/rsvp/Promise.java
@@ -8,11 +8,93 @@ import org.stjs.javascript.functions.Function1;
 
 @Namespace("RSVP")
 public class Promise<Result, FailResult> {
+
+	/**
+	 * Constructs a new Promise instance where the resolution or rejection state is controlled by the specified
+	 * handler function.
+	 * <p/>
+	 * <pre>
+	 * Promise<String, Error> promise = new RSVP.Promise((resolve, reject) {
+	 *   // succeed
+	 *   resolve(value);
+	 *   // or reject
+	 *   reject(error);
+	 * });
+	 *
+	 * promise.then(resolveValue -> {
+	 *     // success
+	 *   },
+	 *   rejectValue -> {
+	 *     // failure
+	 *   }
+	 * );
+	 * </pre>
+	 * <p/>
+	 * Once a promise has been resolved or rejected, it cannot be resolved or rejected again.
+	 * <p/>
+	 * Here is an example of a simple XHR2 wrapper written using RSVP.js:
+	 *
+	 * <pre>
+	 * Function1<String, String> getJSON = url -> {
+	 *   Promise<String, XmlHttpRequest> promise = new RSVP.Promise(resolve, reject -> {
+	 *     XMLHttpRequest client = new XMLHttpRequest();
+	 *     client.open("GET", url);
+	 *     client.onreadystatechange = handler;
+	 *     client.responseType = "json";
+	 *     client.setRequestHeader("Accept", "application/json");
+	 *     client.send();
+	 *
+	 *     Callback0 handler = () -> {
+	 *       if (client.readyState === this.DONE) {
+	 *         if (client.status === 200) {
+	 *           resolve(client.response);
+	 *         } else {
+	 *           reject(client);
+	 *         }
+	 *       }
+	 *     };
+	 *   });
+	 *   return promise;
+	 * };
+	 *
+	 * getJSON.$invoke("/posts.json").then(
+	 *   json -> {
+	 *     // continue
+	 *   },
+	 *   error -> {
+	 *     // handle errors
+	 *   }
+	 * );
+	 * </pre>
+	 */
 	public Promise(Callback2<Callback1<Result>, Callback1<FailResult>> handler) {
 	}
 
+	/**
+	 * Calls the specified callback when the promise is resolved. The specified callback receives the resolution value
+	 * as parameter.
+	 * <p/>
+	 * Then next chained callback will be called as soon as the specified callback finishes.
+	 * <p/>
+	 * The callback does not transform the type of the value passed to the next chained resolution or rejection callbacks.
+	 *
+	 * @param success a callback executed when the promise is resolved.
+	 * @return a promise that can be chained with more callbacks
+	 */
 	public native Promise<Result, FailResult> then(Callback1<? super Result> success);
 
+	/**
+	 * Calls one of the specified callbacks when the promise is resolved or rejected respectively. The specified callbacks
+	 * receives the resolution/rejection values as parameter.
+	 * <p/>
+	 * Then next chained callback will be called as soon as one the specified callback finishes.
+	 * <p/>
+	 * The callbacks does not transform the type of the value passed to the next chained resolution or rejection callbacks.
+	 *
+	 * @param success a callback executed when the promise is resolved.
+	 * @param failure a callback executed when the promise is rejected.
+	 * @return a promise that can be chained with more callbacks
+	 */
 	public native Promise<Result, FailResult> then(Callback1<? super Result> success, Callback1<? super FailResult> failure);
 
 	@Template("suffix(Return)")
